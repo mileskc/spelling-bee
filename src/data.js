42c99061060f07c1c3ce32a2cd864caa49c810a4
Katelyn <katelyncmiles@gmail.com>
2020-04-17 13:50:42 -0400

    letters rendered in cells

---
+++ src/data.js
@@ -0,0 +1,11 @@
+const wordData = [
+  {
+    gameNum: 1,
+    letters: ['l', 'c', 'm', 'n', 'o', 'u', 'y'],
+    wordList: ["uncommonly, cloy, colon, colony, column, commonly, cool, coolly, coyly, cull, loco, loll, loom, loon, loony, lull, lulu, moll, mull, null, nylon, only, uncool"],
+    numWords: 23,
+    pangram: "uncommonly"
+  }
+]
+
+export default wordData
