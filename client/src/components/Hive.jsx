671f6aab69ff5aa5978db04d3f2bf6134c4a4838
Katelyn <katelyncmiles@gmail.com>
2020-04-18 19:15:23 -0400

    input no longer affects center cell

--- client/src/components/Hive.jsx
+++ client/src/components/Hive.jsx
@@ -10,7 +10,6 @@ class Hive extends React.Component{
   constructor(props) {
     super()
     let outerLetters = [...wordData[0].letters]
-    console.log(outerLetters)
     outerLetters.shift()
     this.state={
       centerLetter: [...wordData[0].letters[0]],
@@ -35,13 +34,14 @@ class Hive extends React.Component{
 
   handleShuffle = () => {
     this.shuffleLetters()
-    console.log(this.state.letters)
+    // console.log(this.state.letters)
   }
 
   render() {
   return(
-    <div className = "hive">
+    <div>
       <InputBar/>
+    <div className = "hive">
       <svg className = "hive-cell">
         <polygon className="hex-cell middle" points = "0,52 30,0 90,0 120,52 90,104 30,104" stroke="white">
         </polygon>
@@ -79,6 +79,7 @@ class Hive extends React.Component{
       </svg>
 
       <Shuffle handleShuffle={this.handleShuffle}/>
+    </div>
     </div>
   )
   }
--- client/src/components/InputBar.jsx
+++ client/src/components/InputBar.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import wordData from '../data'
+import '../styles/InputBar.css'
 
 class InputBar extends React.Component {
+ 
+  constructor(props) {
+    super()
+    this.state={
+      currentWord: ""
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      currentWord: event.target.value
+    })
+
+    console.log(this.state.currentWord)
+  }
+
+  handleSubmit = () => {
+    // event.preventDefault()
+    wordData[0].wordList.includes(this.state.currentWord)? console.log("yes") : console.log("no")
+    console.log("called")
+  }
+
   render() {
     return(
       <div>
-        <input/>
+        {/* <form > */}
+          <input onChange={this.handleChange} name="currentWord" value={this.state.currentWord}/>
+          <button onClick={this.handleSubmit}>Enter</button>
+        {/* </form> */}
       </div>
     )
   }
