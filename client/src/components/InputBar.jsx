707bd097720ae4b1ddaf3b28866c6c69fce31b66
Katelyn <katelyncmiles@gmail.com>
2020-04-18 18:38:27 -0400

    added input component

---
+++ client/src/components/InputBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class InputBar extends React.Component {
+  render() {
+    return(
+      <div>
+        <input/>
+      </div>
+    )
+  }
+}
+
+export default InputBar
