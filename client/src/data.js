26dd95b8dc21293249b84469abeceef2f1aba7fd
Katelyn <katelyncmiles@gmail.com>
2020-04-18 23:05:26 -0400

    added second game to data

--- client/src/data.js
+++ client/src/data.js
@@ -5,6 +5,13 @@ const wordData = [
     wordList: ["uncommonly", "cloy", "colon", "colony", "column", "commonly", "cool", "coolly", "coyly", "cull", "loco", "loll", "loom", "loon", "loony", "lull", "lulu", "moll", "mull", "null", "nylon", "only", "uncool"],
     numWords: 23,
     pangram: "uncommonly"
+  },
+  {
+    gameNum: 2,
+    letters: ['a', 'c', 'h', 'p', 't', 'u', 'z'],
+    wordList: ["chutzpah", "attach", "captcha", "catch", "catchup", "chap", "chat", "hatch", "hath", "hatha", "huzzah", "pact", "papa", "patch", "path", "phat", "pupa", "tach", "tact", "tapa", "taut", "that", "thatch"],
+    numWords: 23,
+    pangram: "chutzpah"
   }
 ]
 
