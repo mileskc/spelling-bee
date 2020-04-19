b2842e1fb157eb58f5631bf3efd8008a9605420e
Katelyn <katelyncmiles@gmail.com>
2020-04-18 23:21:25 -0400

    list of correctly guessed words persists

--- client/src/components/InputBar.jsx
+++ client/src/components/InputBar.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import wordData from '../data.js'
 import '../styles/InputBar.css'
 
+let correctWords = []
+
 class InputBar extends React.Component {
  
   constructor(props) {
     super()
     this.state={
       currentWord: "",
-      isValid: false
+      isValid: false,
+      correctWords: correctWords
     }
   }
 
@@ -25,10 +28,13 @@ class InputBar extends React.Component {
     // event.preventDefault()
     // console.log(this.state.currentWord)
     if (wordData[0].wordList.includes(this.state.currentWord)) {
+      correctWords.push(this.state.currentWord)
       this.setState({
-        isValid:true
+        isValid:true,
+        correctWords: correctWords
       })
     }
+    console.log(this.state.correctWords)
     // console.log("called")
   }
 
@@ -43,7 +49,10 @@ class InputBar extends React.Component {
           <input onChange={this.handleChange} name="currentWord" value={this.state.currentWord}/>
           <button onClick={this.checkValidity}>Enter</button>
         {/* </form> */}
-        {this.state.isValid ? <p>{this.state.currentWord}</p>: null}
+        {/* {this.state.isValid ? <p>{this.state.currentWord}</p>: null} */}
+        {this.state.correctWords.map(word=>{
+         return  <p>{word}</p>
+        })}
       </div>
     )
   }
