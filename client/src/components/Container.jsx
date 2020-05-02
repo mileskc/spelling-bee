41e4a33c3d67fd459c5186e1b1218b8074ff13e5
Katelyn <katelyncmiles@gmail.com>
2020-05-02 12:51:30 -0400

    added container and api config

---
+++ client/src/components/Container.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Container extends React.Component {
+  render(){
+    return(
+      <>
+      {/* <Routes/> */}
+      </>
+
+    )
+  }
+}
+
+export default Container
