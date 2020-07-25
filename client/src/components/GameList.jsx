bfb2cb4ad828e0380a9bdcd6d08cdea16f30ec9a
Katelyn <katelyncmiles@gmail.com>
2020-07-24 22:51:26 -0400

    add prod baseurl for gamelist

--- client/src/components/GameList.jsx
+++ client/src/components/GameList.jsx
@@ -3,6 +3,12 @@ import axios from 'axios'
 import '../styles/GameList.css'
 import { Link } from 'react-router-dom'
 
+let baseURL
+if (process.env.NODE_ENV === 'development') {
+  baseURL = 'http://localhost:3000'
+} else {
+  baseURL = 'http://spelling-bee-clone-backend.herokuapp.com'
+}
 class GameList extends React.Component {
   constructor() {
     super()
@@ -17,7 +23,7 @@ class GameList extends React.Component {
   }
 
   getAllGames = async () => {
-    const resp = await axios.get(`http://localhost:3000/api/games`)
+    const resp = await axios.get(`${baseURL}/api/games`)
     this.setState({
       games: resp.data.games
     })
@@ -29,7 +35,7 @@ class GameList extends React.Component {
         <h1 className="welcome-user">Welcome back, <span>{this.props.user.username}</span>!</h1>
         < div className="game-list">
           {this.state.games.map(game =>
-            <Link className="game-link" to={`/game-list/${game.gameNum}`}>
+            <Link className="game-link" to={`/ game - list / ${game.gameNum}`}>
               <div className="game-thumbnail">
                 <img id="game-photo" src="https://i.imgur.com/PRRP96C.png" />
                 <h1 className="game-num">Game {game.gameNum}</h1>
